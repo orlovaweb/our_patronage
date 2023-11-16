e9b8cd0321242e1367c1e2994c816f66669f02d8
l.orlova <igimon@mail.ru>
2023-11-17 00:23:44 +0300

    fix bag aos

--- js/main.js
+++ js/main.js
@@ -47,6 +47,13 @@ $(function () {
     // $('.header__call').hide();
   });
 
+  if (document.documentElement.clientWidth <= 1100) {
+
+    $('.menu__item').on('click', function () {
+      $('.header__menu').hide();
+    });
+  }
+
 
 
   /**
@@ -60,4 +67,24 @@ $(function () {
     newsBlock.innerHTML = html;
   }
 });
+// плавный скролл
+document.querySelectorAll('a[href^="#"').forEach(link => {
 
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    let href = this.getAttribute('href').substring(1);
+
+    const scrollTarget = document.getElementById(href);
+
+    const topOffset = document.querySelector('.header').offsetHeight;
+    // const topOffset = 0; // если не нужен отступ сверху
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
+  });
+});
